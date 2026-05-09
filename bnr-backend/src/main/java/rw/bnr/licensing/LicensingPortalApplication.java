90aa12986f41c1f2ccf540b561124db0e7ca756b
David NTAMAKEMWA <david.ntamakemwa@bnr.rw>
2026-05-10 00:15:36 +0200

    Backend: initial Spring Boot 3.3 project setup with PostgreSQL, Flyway, JWT dependencies

---
+++ bnr-backend/src/main/java/rw/bnr/licensing/LicensingPortalApplication.java
@@ -0,0 +1,15 @@
+package rw.bnr.licensing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author David NTAMAKEMWA
+ */
+@SpringBootApplication
+public class LicensingPortalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LicensingPortalApplication.class, args);
+    }
+}
